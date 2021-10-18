058b9eeb93c82ab901be0dab6b5572621b3b48aa
Kevin Arifin <kevarifin14@gmail.com>
2021-10-18 13:59:05 -0700

    add Google Analytics to template

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,31 @@
 import 'tailwindcss/tailwind.css'; // eslint-disable-line import/no-extraneous-dependencies
 import { DefaultSeo } from 'next-seo';
 import dynamic from 'next/dynamic';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 import WalletsProvider from 'contexts/wallets';
+import { pageview } from 'utils/ga';
 
 const WalletConnectionProvider = dynamic(() => import('contexts/walletConnection'), {
   ssr: false,
 });
 
 function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') {
+      router.events.on('routeChangeComplete', pageview);
+    }
+
+    return () => {
+      if (process.env.NODE_ENV === 'production') {
+        router.events.off('routeChangeComplete', pageview);
+      }
+    };
+  }, [router.events]);
+
   const title = 'title';
   const description = 'description';
 
